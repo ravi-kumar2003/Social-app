812660c8e9d21cd719cf1ac7defa669093190879
Ravi kumar <ravisah62020@gmail.com>
2026-02-06 01:10:32 +0530

    Fix api server

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_SERVER_URL + "/api" ,
 });
 
-// attach token automatically
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
 
